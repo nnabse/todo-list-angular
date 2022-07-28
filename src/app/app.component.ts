4e03b8317c28d8d2099b533dd6f9ddb7b47953a7
nnabse <nnabse@gmail.com>
2022-07-28 15:43:09 +0300

    icons, add task function

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,8 +3,22 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
   title = 'todo-list-angular';
+
+  allTasks: any[] = [{ taskName: 'Учить Angular', isDone: false }];
+
+  addTodoForm: any = {
+    taskName: 'Учить Angular',
+    isDone: false,
+  };
+
+  printForm() {
+    this.allTasks.push({ taskName: this.addTodoForm.taskName, isDone: false });
+    console.log(this.allTasks);
+    this.addTodoForm.taskName = '';
+  }
+  render() {}
 }
