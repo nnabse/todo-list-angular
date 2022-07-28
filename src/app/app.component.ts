a333cbbddab883fd97189e5878a4a9995d5d4456
nnabse <nnabse@gmail.com>
2022-07-28 15:49:57 +0300

    fix

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -8,7 +8,7 @@ import { Component } from '@angular/core';
 export class AppComponent {
   title = 'todo-list-angular';
 
-  allTasks: any[] = [{ taskName: 'Учить Angular', isDone: false }];
+  allTasks: any[] = [];
 
   addTodoForm: any = {
     taskName: 'Учить Angular',
