f081a37ed0b7f929b722eb4db6dd2bc42bd8e1e2
nnabse <nnabse@gmail.com>
2022-07-29 10:48:37 +0300

    delete task feature

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -10,19 +10,34 @@ export class AppComponent {
 
   allTasks: taskInterface[] = [];
 
+  id: number = 0;
+
   addTodoForm: taskInterface = {
+    id: 0,
     taskName: '',
     isDone: false,
   };
 
-  printForm() {
-    this.allTasks.push({ taskName: this.addTodoForm.taskName, isDone: false });
+  addTask() {
+    this.allTasks.push({
+      id: this.id++,
+      taskName: this.addTodoForm.taskName,
+      isDone: false,
+    });
     this.addTodoForm.taskName = '';
+  }
+
+  removeTask(itemId: number) {
+    this.allTasks.forEach((elem, index) => {
+      if (elem.id === itemId) this.allTasks.splice(index, 1);
+    });
+    console.log(this.allTasks);
   }
   render() {}
 }
 
 interface taskInterface {
+  id: number;
   taskName: string;
   isDone: boolean;
 }
